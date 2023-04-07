5c66d230f3c219dd229f16bac6d6d922726002dd
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2023-04-07 10:18:31 +0530

    installed react router,framer motion,react-icons styled-components,react-splide,@splidejs/react-splide  implemented Splide and SplideSlide

--- delicious-recipe-app/src/components/Popular.jsx
+++ delicious-recipe-app/src/components/Popular.jsx
@@ -1,20 +1,54 @@
-import React,{useEffect} from 'react'
-
+import React, { useEffect, useState } from 'react';
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 function Popular() {
-
-    useEffect(()=>{
+    const [popular, setPopular] = useState([]);
+    useEffect(() => {
         getPopular();
-    },[])
-    const getPopular = async () =>{
+    }, [])
+    const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-        const data = api.json()
-        console.log(data)
+        const data = await api.json()
+        console.log("data =>", data)
+        setPopular(data.recipes)
+        console.log("popular =>", popular)
+
     }
-  return (
-    <div>
-      <h1>popular</h1>
-    </div>
-  )
+    return (
+        <div>
+                <Wrapper>
+                    <h3>Popular Picks</h3>
+                    <Splide options={{
+                        perPage: 4
+                    }}>
+                        {popular.map((recipe) => {
+                            return (
+                                <SplideSlide>
+                                    <Card >
+                                        <p>{recipe.title}</p>
+                                        <img src={recipe.image} alt={recipe.title} />
+                                    </Card>
+                                </SplideSlide>
+                            );
+                        })}
+                    </Splide>
+                </Wrapper>
+        </div>
+    )
 }
 
+
+const Wrapper = styled.div`
+margin: 4rem 0rem;`
+
+const Card = styled.div`
+   min-height: 25rem;
+   border-radius: 2rem;
+   overflow: hidden;
+
+   img{
+    border-radius: 2rem;
+   }
+`;
 export default Popular;
